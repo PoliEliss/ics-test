a61ee65fe8360358532a22b999f275ce9fb53261
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2023-04-13 13:19:54 +0300

    Update dbo.usp_MakeFamilyPurchas.sql
    CREATE PROCEDURE dbo.usp_MakeFamilyPurchase

--- dbo.usp_MakeFamilyPurchas.sql
+++ dbo.usp_MakeFamilyPurchas.sql
@@ -1,1 +1,17 @@
 
+CREATE PROCEDURE [dbo].[usp_MakeFamilyPurchase]
+    @FamilySurName varchar(255)
+AS
+BEGIN
+    
+    IF NOT EXISTS (SELECT * FROM dbo.Family WHERE SurName = @FamilySurName)
+    BEGIN
+      PRINT 'Такой семьи нет'
+       RETURN
+   END
+    DECLARE @TotalPurchase INT
+    SELECT @TotalPurchase = SUM(Value) FROM dbo.Basket WHERE ID_Family = @FamilySurName
+    UPDATE dbo.Family
+    SET BudgetValue = BudgetValue - @TotalPurchase
+    WHERE SurName = @FamilySurName
+END
